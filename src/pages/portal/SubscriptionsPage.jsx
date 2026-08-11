c86f644b43606c21868c5c7b1b49c76ac8f0c097
Wladislaw <65966048+VlAcosta@users.noreply.github.com>
2026-08-12 02:50:04 +0300

    ux: show contextual upgrade reason above tariff workspace

--- src/pages/portal/SubscriptionsPage.jsx
+++ src/pages/portal/SubscriptionsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PortalLayout from '../../layouts/PortalLayout';
 import { SubscriptionsWorkspace } from '../../features/subscriptions';
+import SubscriptionUpgradeContext from '../../features/subscriptions/SubscriptionUpgradeContext/SubscriptionUpgradeContext';
 
 export default function SubscriptionsPage() {
   return (
     <PortalLayout title="Тариф и оплата" subtitle="Настройки">
+      <SubscriptionUpgradeContext />
       <SubscriptionsWorkspace />
     </PortalLayout>
   );
